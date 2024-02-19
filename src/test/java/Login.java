1677b8e1543cca645613eb9e2ed26c86c2496d3e
Aditya <aditya1995.jr@gmail.com>
2024-02-19 19:51:12 +0700

    Setting environtment and add element for login page

--- src/test/java/Login.java
+++ src/test/java/Login.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.time.Duration;
 
 public class Login extends env_target {
