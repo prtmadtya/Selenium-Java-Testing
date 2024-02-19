e824c6a5dea009c9aa152d5e4bf1960674648d64
Aditya <aditya1995.jr@gmail.com>
2024-02-19 15:17:31 +0700

    Add login java

---
+++ src/main/java/config/env_target.java
@@ -0,0 +1,7 @@
+package config;
+import org.openqa.selenium.WebDriver;
+
+public class env_target {
+    protected WebDriver driver;
+    protected String baseUrl = "https://www.saucedemo.com/";
+}
---
+++ src/test/java/Login.java
@@ -0,0 +1,9 @@
+import config.env_target;
+import org.junit.jupiter.api.Test;
+
+public class Login {
+    @Test
+    public void main(){
+        System.setProperty("webdriver.chrome.driver","src\\main\\resources\\chromedriver.exe");
+    }
+}
