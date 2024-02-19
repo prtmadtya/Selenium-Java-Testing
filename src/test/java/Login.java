814ce15ad6655c46a5529aa87424ca19bad74145
Aditya <aditya1995.jr@gmail.com>
2024-02-19 19:38:34 +0700

    Setting environtment and add element for login page

--- src/test/java/Login.java
+++ src/test/java/Login.java
@@ -1,9 +1,36 @@
 import config.env_target;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class Login {
+import java.time.Duration;
+
+public class Login extends env_target {
     @Test
     public void main(){
+        //Set Chromedriver location path
         System.setProperty("webdriver.chrome.driver","src\\main\\resources\\chromedriver.exe");
+        driver = new ChromeDriver();
+        //Maximazie minimize
+        driver.manage().window().maximize();
+        //get URL
+        driver.get(baseUrl);
+        //set Duration wait showing login page
+        Duration duration = Duration.ofSeconds(10);
+        WebDriverWait wait = new WebDriverWait(driver, duration);
+        wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@type='submit'] [@id='login-button']"))
+        );
+        //Assertion
+        driver.findElement(By.name("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.xpath("//input[@type='submit'] [@id='login-button']")).click();
+        //Wait until
+        wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"header_container\"]/div[2]/span"))
+        );
+        driver.quit();
     }
 }
